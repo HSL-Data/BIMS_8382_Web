5fa2c96f6ff43f2e06e322df90ddd494819ca443
dnm5ca <41120025+dnm5ca@users.noreply.github.com>
2019-02-07 15:17:22 -0500

    test

--- public/Week 2 - Dealing with Data/DealingWithData.R
+++ public/Week 2 - Dealing with Data/DealingWithData.R
@@ -4,5 +4,5 @@
 #' output: html_document
 #' ---
 #'
-#'[Back to the main page](bims8382.netlify.com)
+#'[Back to the main page](https://bims8382.netlify.com)
 #'This site will be up at the end of our Week 1 class.
